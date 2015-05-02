4a14a5bfc1189aeb78e6b51a0e8f69be4d593094
David Begin <davidmichaelbe@gmail.com>
2015-05-02 15:44:09 -0700

    add another toolbox and namespace the specs

--- src/active_vector.rs
+++ src/active_vector.rs
@@ -1,3 +1,23 @@
+pub mod longest_toolbox {
+    pub fn length(vec: Vec<&str>) -> usize {
+        longest_option(vec).unwrap_or("").len()
+    }
+
+    fn longest_option(vec: Vec<&str>) -> Option<&str> {
+        if vec.len() > 0 {
+            let mut longest = vec[0];
+            for element in vec.iter() {
+                if element.len() > longest.len() {
+                    longest = *element;
+                }
+            }
+            Some(longest)
+        } else {
+            None
+        }
+    }
+}
+
 pub mod shortest_toolbox {
     pub fn length(names: Vec<&str>) -> usize {
         shortest_option(names).unwrap_or("").len()
--- src/lib.rs
+++ src/lib.rs
@@ -5,24 +5,31 @@ mod active_vector;
 
 #[cfg(test)]
 mod tests {
-    use super::active_vector::shortest_toolbox::length;
 
-    #[test]
-    fn shortest_toolbox_length_returns_the_shortest_length() {
-        let names = vec!["He", "Lee"];
-        assert_eq!(length(names), 2);
+    mod longest_toolbox_tests {
+        use active_vector::longest_toolbox::length;
+
+        #[test]
+        fn length_returns_the_longest_length() {
+            let vec = vec!["He", "Lee"];
+            assert_eq!(length(vec), 3);
+        }
     }
 
-    #[test]
-    fn shortest_toolbox_length_returns_the_shortest_length_2() {
-        let names = vec!["Lee", "Smith"];
-        assert_eq!(length(names), 3);
-    }
+    mod shortest_toolbox_tests {
+        use active_vector::shortest_toolbox::length;
 
-    #[test]
-    fn shortest_toolbox_length_returns_the_shortest_length_3() {
-        let names = vec![];
-        assert_eq!(length(names), 0);
+        #[test]
+        fn length_returns_the_shortest_length() {
+            let names = vec!["He", "Lee"];
+            assert_eq!(length(names), 2);
+        }
+
+        #[test]
+        fn length_returns_0_if_the_vector_is_empty() {
+            let names = vec![];
+            assert_eq!(length(names), 0);
+        }
     }
 }
 
