f66f62222f2aa9098481667c47a4b5eee9cfff61
David Begin <davidmichaelbe@gmail.com>
2015-05-02 14:35:37 -0700

    refactoring using unwrap_or

--- src/active_vector.rs
+++ src/active_vector.rs
@@ -1,16 +1,9 @@
 pub mod shortest_toolbox {
     pub fn print(names: Vec<&str>) {
-        println!("Shortest Name: {:?}", name(names));
+        println!("{:?}", shortest_option(names).unwrap_or("Not Found"));
     }
 
-    fn name(names: Vec<&str>) -> String {
-        match option(names) {
-            Some(shortest) => shortest.to_string(),
-            _              => "Not Found".to_string(),
-        }
-    }
-
-    fn option(names: Vec<&str>) -> Option<&str> {
+    fn shortest_option(names: Vec<&str>) -> Option<&str> {
         if names.len() > 0 {
             let mut shortest = names[0];
             for name in names.iter() {
