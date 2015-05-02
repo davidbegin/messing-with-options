fa5a94c1a1fdf7c93e53c69563579a754ccba307
David Begin <davidmichaelbe@gmail.com>
2015-05-02 15:22:58 -0700

    adding some unit specs, and some silly namespacing

--- src/active_vector.rs
+++ src/active_vector.rs
@@ -1,4 +1,8 @@
 pub mod shortest_toolbox {
+    pub fn length(names: Vec<&str>) -> usize {
+        shortest_option(names).unwrap_or("").len()
+    }
+
     pub fn print(names: Vec<&str>) {
         println!("{:?}", shortest_option(names).unwrap_or("Not Found"));
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
-#[test]
-fn it_works() {
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+mod active_vector;
+
+#[cfg(test)]
+mod tests {
+    use super::active_vector::shortest_toolbox::length;
+
+    #[test]
+    fn shortest_toolbox_length_returns_the_shortest_length() {
+        let names = vec!["He", "Lee"];
+        assert_eq!(length(names), 2);
+    }
+
+    #[test]
+    fn shortest_toolbox_length_returns_the_shortest_length_2() {
+        let names = vec!["Lee", "Smith"];
+        assert_eq!(length(names), 3);
+    }
+
+    #[test]
+    fn shortest_toolbox_length_returns_the_shortest_length_3() {
+        let names = vec![];
+        assert_eq!(length(names), 0);
+    }
 }
+
+
